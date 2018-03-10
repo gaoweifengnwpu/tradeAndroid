c6ba6e69ccb878b0e47dbc9e48c501b4c216a312
GWF <2032317699@qq.con>
2018-03-10 13:16:30 +0800

    trade

--- login/src/main/java/com/fedming/bottomnavigationdemo/BottomNavigationViewHelper.java
+++ login/src/main/java/com/fedming/bottomnavigationdemo/BottomNavigationViewHelper.java
@@ -9,6 +9,7 @@ import java.lang.reflect.Field;
 // 利用反射，改变 item 中 mShiftingMode 的值
 public class BottomNavigationViewHelper {
 
+
     public static void disableShiftMode(BottomNavigationView navigationView) {
 
         BottomNavigationMenuView menuView = (BottomNavigationMenuView) navigationView.getChildAt( 0 );
